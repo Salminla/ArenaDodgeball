d0d8407c90ae93a350d2b4d77769c3cc12c8b5cd
bripe003 <bripe003@edu.xamk.fi>
2021-12-09 13:20:06 +0200

    Rotating Objects

--- Assets/_project/Scripts/RotatingObject.cs
+++ Assets/_project/Scripts/RotatingObject.cs
@@ -16,6 +16,6 @@ public class RotatingObject : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.forward * rotateSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
     }
 }
